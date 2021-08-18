fe49c00aa7b2accb8047c1adaa87b2643a72ab88
ProjectVH <projectvaluehub@outlook.com>
2021-08-18 09:06:03 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"projectvaluehub@outlook.com\"\n\
+echo "
+[general]n
+email = "projectvaluehub@outlook.com"n
 " > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "
+[server]n
+headless = truen
+enableCORS=falsen
+port = $PORTn
 " > ~/.streamlit/config.toml
